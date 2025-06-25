bc53c1843de2acd6a7dfa14124493ceca1c2a560
PKhant78 <phonekhantkyawswa555@gmail.com>
2025-06-24 20:04:44 -0400

    Added Gifs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React, {useEffect, useState}from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { createRoot } from "react-dom/client";
-// import axios from "axios"; // Maybe we'll need axios? 🤔
+
 import "./style.css";
 import SearchField from "./SearchField";
 import GifCard from "./GifCard";
@@ -12,21 +12,21 @@ const App = () => {
   const [data, setData] = useState([]);
 
   const fetchData = async (searchValue) => {
-    const response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchValue}&api_key=${GIPHY_API_KEY}`)
-    setData(response.data.data);
-    // console.log("This is data-->", response.data)
-    // console.log("This is Search Value-->", searchValue)
+    const response = await axios.get(
+      `http://api.giphy.com/v1/gifs/search?q=${searchValue}&api_key=${GIPHY_API_KEY}`
+    );
+    console.log(response.data);
+    setData(response.data.data); // set the arrays of GIF objects into data
   };
-  // console.log("this is Data-->", data)
 
   useEffect(() => {
     fetchData();
-  }, [])
+  }, []);
   return (
     <div className="app">
-      <h1 className="title">Let's Make Some API Requests!</h1>
-      <SearchField fetchData = {fetchData}/>
-      <GifCard data = {data}/>
+      <h1 className="title">Search GIFS!</h1>
+      <SearchField fetchData={fetchData} />
+      <GifCard data={data} />
     </div>
   );
 };
--- src/GifCard.jsx
+++ src/GifCard.jsx
@@ -1,11 +1,26 @@
 import React from "react";
-import './GifCard.css'
+import "./GifCard.css";
 
-const GifCard = ({data}) => {
-  console.log("this is data-->", data[0]?.title)
+const GifCard = ({ data }) => {
+  console.log("this is data-->", data[0]?.images);
+  console.log("this is data-->", data[0]);
+
+  //slice from 1 to 10
   return (
     <div className="gif-card-container">
-      <div className="cards">{}</div>
+      {data.slice(1, 10).map((gif) => (
+        <div className="cards" key={gif?.id}>
+          <img src={gif?.images?.fixed_height?.url} alt={gif?.alt_text} />
+        </div>
+      ))}
+    </div>
+  );
+};
+/*
+    <div className="gif-card-container">
+      <div className="cards">
+        <img src={data[0]?.images?.preview_gif.url}></img>
+      </div>
       <div className="cards">ghdgh</div>
       <div className="cards">dfghdh</div>
       <div className="cards">dfghd</div>
@@ -17,9 +32,6 @@ const GifCard = ({data}) => {
       <div className="cards">hdgh</div>
       <div className="cards"></div>
       <div className="cards"></div>
-
-    </div>
-  )
-};
+    </div>*/
 
 export default GifCard;
