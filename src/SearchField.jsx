31f46d7f295729857edabf7210353b25f6886871
PKhant78 <phonekhantkyawswa555@gmail.com>
2025-06-24 20:25:56 -0400

    Fixed reloading page when enter/return is pressed in the search field. Implemented the ablity to directly search gif using enter/return instead of pressing submit button

--- src/SearchField.jsx
+++ src/SearchField.jsx
@@ -1,30 +1,35 @@
-import React ,{useState}from "react";
-import './SearchField.css'
+import React, { useState } from "react";
+import "./SearchField.css";
 
-const SearchField = ({fetchData}) => {
-  const [searchValue, setSearchValue] = useState()
+const SearchField = ({ fetchData }) => {
+  const [searchValue, setSearchValue] = useState();
 
   const handleInput = (event) => {
-    setSearchValue(event.target.value)
-  }
+    setSearchValue(event.target.value);
+  };
 
   const handleSubmit = () => {
-    fetchData(searchValue)
-  }
+    fetchData(searchValue);
+  };
   return (
     <main>
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault(); // prevent page from reloading when enter/return is pressed
+          handleSubmit();
+        }}
+      >
         <input
-        className="search-box"
-        type='text'
-        placeholder="What kind of GIFs are you looking for?"
-        value={searchValue}
-        onChange={handleInput}/>
+          className="search-box"
+          type="text"
+          placeholder="What kind of GIFs are you looking for?"
+          //value={searchValue} // probably not needed
+          onChange={handleInput}
+        />
       </form>
       <button onClick={handleSubmit}>Submit</button>
     </main>
   );
 };
 
-// 3uPYTPfMF80n8U1q1PLoIu75lKpbeFzk
 export default SearchField;
